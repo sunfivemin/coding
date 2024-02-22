c9af28e0dbcd2cbad9c782911f068a7675141dfb
sunfivemin <154585975+sunfivemin@users.noreply.github.com>
2024-02-22 16:02:24 +0900

    JS-projects

---
+++ src/main.js
@@ -0,0 +1,32 @@
+"use strict";
+
+// Header에 페이지 아래로 스크롤시 다크 스타일링 적용
+const header = document.querySelector(".header");
+const headerRect = header.getBoundingClientRect();
+const headerHeight = header.offsetHeight;
+
+document.addEventListener("scroll", () => {
+    if (window.scrollY > headerHeight) {
+        header.classList.add("header-dark");
+    } else {
+        header.classList.remove("header-dark");
+    }
+});
+
+// Home 섹션을 아래로 스크롤시 투명하게 처리함
+const home = document.querySelector(".home_container");
+const homeHeight = home.offsetHeight;
+document.addEventListener("scroll", () => {
+    home.style.opacity = 1 - window.scrollY / homeHeight;
+});
+
+// Arrow up버튼을 아래로 스크롤시 투명하게 처리함
+const arrowUp = document.querySelector(".arrow-up");
+document.addEventListener("scroll", () => {
+    if (window.scrollY > homeHeight / 2) {
+        arrowUp.style.opacity = 1;
+    } else {
+        arrowUp.style.opacity = 0;
+    }
+});
+
