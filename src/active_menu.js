e4ade35d38da95b474fdac80c83f10c1ad2d5ac4
sunfivemin <154585975+sunfivemin@users.noreply.github.com>
2024-02-22 17:44:39 +0900

    observerAPI

---
+++ src/active_menu.js
@@ -0,0 +1,38 @@
+const sectionIds = ["#home", "#work"];
+const sections = sectionIds.map((id) => document.querySelector(id));
+const navItems = sectionIds.map((id) => document.querySelector(`[href="${id}"]`));
+
+const visibleSections = sectionIds.map(() => false);
+let activeNavItem = navItems[0];
+
+const options = {
+    rootMargin: "-20% 0px 0px 0px",
+    threshold: [0, 0.98],
+};
+const observer = new IntersectionObserver(observerCallback, options);
+sections.forEach((section) => observer.observe(section));
+
+function observerCallback(entries) {
+    let selectLastOne; // flag 변수 (true인지 아닌지 알려주는 변수)
+    entries.forEach((entry) => {
+        const index = sectionIds.indexOf(`#${entry.target.id}`);
+        visibleSections[index] = entry.isIntersecting;
+        selectLastOne = index === sectionIds.length - 1 && entry.isIntersecting && entry.intersectionRatio >= 0.95;
+    });
+
+    const navIndex = selectLastOne ? sectionIds.length - 1 : findFirstIntersecting(visibleSections);
+    selectNavItem(navIndex);
+}
+
+function findFirstIntersecting(intersections) {
+    const index = intersections.indexOf(true);
+    return index >= 0 ? index : 0;
+}
+
+function selectNavItem(index) {
+    const navItem = navItems[index];
+    if (!navItem) return;
+    activeNavItem.classList.remove("active");
+    activeNavItem = navItem;
+    activeNavItem.classList.add("active");
+}
